f4a47807e638fe9744ebcb336c993bf26b93e2e2
MeiC-0 <mchan1@mail.pima.edu>
2024-09-09 22:11:36 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+#Author: Mei Li Chan
+#My first python lab in my CIS129 course
+#Prints a simple short story
+print('A duck walked up to a lemonade stand and he said to the man running the stand "hey, got any grapes?" the man said no so he waddled away.')
+print('The very next day the duck came up to the lemonade stand and said "hey, got any limes?" the man said no so he waddled away.')
+print('The very next day the duck came up to the lemonade stand and said "hey, got any lemonade?" the man said no so he waddled away.')
