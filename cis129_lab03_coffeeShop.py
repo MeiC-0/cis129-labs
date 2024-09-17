631099bd5633a51b2fe4533f43d4c193b5cf0cfd
MeiC-0 <mchan1@mail.pima.edu>
2024-09-16 19:40:16 -0700

    Update cis129_lab03_coffeeShop.py

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -4,20 +4,31 @@
 #put receipt statements together
 #add total + sales tax for overall total
 print("***************************************")
-print("My Coffee and Muffin Shop")
+print("My Cafe")
 print("Number of Coffees Bought?")
 num_coffee = int(input())
 (num_coffee)
 print("Number of Muffins Bought?")
 num_muffin = int(input())
 (num_muffin)
+print('Number of Bagels Bought?')
+num_bagel = int(input())
+(num_bagel)
+print('Number of Cakes Bought?')
+num_cake = int(input())
+(num_cake)
 print("***************************************\n\n***************************************")
-print("My Coffee and Muffin Shop Receipt")
+print("My Cafe Receipt")
 total_coffee = 5 * num_coffee
 total_muffin = 4 * num_muffin
+total_bagel = 3.50 * num_bagel
+total_cake = 3 * num_cake
 print(num_coffee, 'Coffee at $5 each: $', format(total_coffee,'.2f'))
 print(num_muffin, 'Muffin at $4 each: $', format(total_muffin,'.2f'))
-print('6% sales tax: $', round((total_coffee + total_muffin) * .06 ,2))
+print(num_bagel, 'Bagel at $3.50 each: $', format(total_bagel, '.2f'))
+print(num_cake, 'Cakes at $3 each: $', format(total_cake, '.2f'))
+print('6% sales tax: $', round((total_coffee + total_muffin + total_bagel + total_cake) * .06 ,2))
 print('---------')
-print('Total: $', total_coffee + total_muffin + round((total_coffee + total_muffin) * .06 ,2))
+print('Total: $', total_coffee + total_muffin + total_bagel + total_cake + round((total_coffee + total_muffin + total_bagel + total_cake) * .06 ,2))
 print("***************************************")
+print("Thank you for stopping by can't wait to see you next time!")
